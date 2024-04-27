a1bd867c798c59c80488c5a1a05162967ba25cbf
Thomas <th.fchs@gmail.com>
2024-04-27 16:41:02 +0200

    basic function and variables

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 'use strict';
 
+var nom; 
+var life, money; 
+var awake = true;
+
+var run, fight, work, sleep, eat, show, newLife, kill;
+
+function init(n, l, m)
+{
+    nom = n; 
+    life = l;
+    money = m;
+}
+
+
+function showMe()
+{
+    alert(`nom : ${nom}\n life : ${life}\n money : ${money}\n`);
+}
+
+
+
+init("John Doe", 100, 1e6);
+
+showMe();
