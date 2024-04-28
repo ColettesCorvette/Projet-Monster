c0b96685fbc9b4e27213657d286957ab49ba3ddf
Thomas <th.fchs@gmail.com>
2024-04-28 13:47:37 +0200

    oof

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var nom;
 var life, money; 
 var awake = true;
 
-var run, fight, work, sleep, eat, newLife, kill;
+var newLife, kill;
+
+var run = document.getElementById("b2");
+var fight = document.getElementById("b3");
+var work = document.getElementById("b7");
+var sleep = document.getElementById("b4");
+var eat = document.getElementById("b5");
 var show = document.getElementById("b6");
 
 function init(n, l, m)
@@ -17,20 +23,25 @@ function init(n, l, m)
 
 function showMe()
 {
-    alert(`nom : ${nom}\n life : ${life}\n money : ${money}\n`);
+    alert(`nom : ${nom}\n life : ${life}\n money : $${money}\n`);
 }
 
 
 function go()
 {
+
     init("John Doe", 100, 1e6);
-    
+
     show.addEventListener('click', event => {
-            showMe();
+        showMe();
     });
     
 }
 
+function log(message)
+{
+
+}
 
 
-go();
+window.onload = go();
