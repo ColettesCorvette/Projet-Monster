4b5481c79140dcc9141fff395f6c062a69fa351c
Thomas <th.fchs@gmail.com>
2024-05-12 09:29:54 +0200

    oof

--- app.js
+++ app.js
@@ -17,7 +17,12 @@ var show = document.getElementById("b6");
 var actionbox = document.getElementById("actionbox");
 
 
-
+/**
+ * Initialise les paramètres du monstre
+ * @param {nom du monstre} n 
+ * @param {nombre de points de vie} l 
+ * @param {somme d'argent de départ du monstre} m 
+ */
 function init(n, l, m)
 {
     nom = n; 
@@ -25,13 +30,17 @@ function init(n, l, m)
     money = m;
 }
 
-
+/**
+ * Affiche les logs du monstre (nom, points de vie, argent)
+ */
 function showMe()
 {   
     log(`nom : ${nom}\n life : ${life}\n money : $${money}\n`);
 }
 
-
+/**
+ * Initialise le monstre et ajoute un event listener sur le bouton "show"
+ */
 function go()
 {
 
@@ -43,6 +52,10 @@ function go()
     
 }
 
+/**
+ * Creer un nouveau paragraphe et l'insère en tête dans la div actionbox
+ * @param {message du log} message 
+ */
 function log(message)
 {   
     var nouveauMessage = document.createElement("p");
@@ -50,6 +63,14 @@ function log(message)
     actionbox.insertBefore(nouveauMessage, actionbox.firstChild);
 }
 
+/**
+ * Met à jour l'affichage des paramètres du monstre
+ * Change la couleur de fond du monstre selon le nombre de points de vie
+ * Change l'épaisseur de la bordure du monstre selon la somme d'argent
+ * @param {vie du monstre} life 
+ * @param {argent du monstre} money 
+ * @param {statut du monstre : réveillé ou endormi} awake 
+ */
 function displayStatus(life, money, awake)
 {   
     var status = document.getElementById("status");
@@ -66,6 +87,10 @@ function displayStatus(life, money, awake)
 
 }
 
+/**
+ * Met à jour la vie du monstre et affiche un message si le monstre est mort, 
+ * dort ou a perdu un point de vie
+ */
 function run()
 {   
 
@@ -80,6 +105,10 @@ function run()
         log("RUN - impossible car le monstre dort");
 }
 
+/**
+ * Met à jour la vie du monstre et affiche un message si le monstre est mort, 
+ * dort ou a perdu des points de vie
+ */
 function fight()
 {
     if (awake && life > 3) {
@@ -93,6 +122,10 @@ function fight()
         log("FIGHT - impossible car le monstre dort");
 }
 
+/**
+ * Met à jour la somme d'argent et la vie du monstre et affiche un message si 
+ * le monstre est mort, dort, n'a pas assez d'argent ou a perdu un point de vie
+ */
 function work()
 {
     if (awake && life > 0)
@@ -108,7 +141,10 @@ function work()
         log("WORK - impossible car le monstre dort");
 }
 
-
+/**
+ * Met à jour la somme d'argent et la vie du monstre et affiche un message si 
+ * le monstre est mort, dort, n'a pas assez d'argent ou a perdu des point de vie
+ */
 function eat()
 {
     if (awake && money > 3 && life>0)
@@ -126,6 +162,9 @@ function eat()
         log("EAT - impossible car le monstre dort");
 }
 
+/**
+ * Timer de 7 secondes pour mettre le monstre en mode sommeil
+ */
 function sleep()
 {
     if(awake)
@@ -145,6 +184,10 @@ function sleep()
 }
 
 
+/**
+ * Fonction qui choisit aléatoirement une action parmi les 5 actions possibles
+ * (run, fight, work, eat, sleep) et l'exécute toutes les 12 secondes
+ */
 function hasard()
 {   
     const tab = [run, fight, work, eat, sleep];
@@ -155,6 +198,9 @@ function hasard()
 
 }
 
+/**
+ * Ressuscite le monstre si il est mort
+ */
 function newLife()
 {   
     if (life === 0)
@@ -167,6 +213,9 @@ function newLife()
         log("NEW LIFE - impossible car le monstre est deja vivant");
 }
 
+/**
+ * Tue le monstre s'il est vivant
+ */
 function kill()
 {   
     if (life > 0) {
@@ -177,6 +226,7 @@ function kill()
     else
         log("KILL - impossible car le monstre est deja mort");
 }
+
 
 
 window.onload = go();
